ef35aec0682d1e0e2249993b4c187ddf8953074d
k-koech <kelvin.koech@student.moringaschool.com>
2021-09-26 21:53:25 +0300

    created test file

--- accounts/views/auth.py
+++ accounts/views/auth.py
@@ -1,6 +1,6 @@
 from django.http.response import JsonResponse
 from django.shortcuts import render, redirect
-from ..models import Neighborhood, Users,Post
+from ..models import Business, Neighborhood, Users,Post
 from django.contrib.auth import authenticate,login,logout,login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -9,8 +9,9 @@ from django.contrib.auth.hashers import make_password
 
 # Create your views here.
 def index(request):
+    businesses= Business.objects.filter(neighborhood__id=request.user.neighborhood.id)
     posts= Post.objects.filter(neighborhood__id=request.user.neighborhood.id)
-    return render(request,'index.html',{"posts":posts})
+    return render(request,'index.html',{"posts":posts, "businesses":businesses})
 
 def dashboard(request):
     """dashboard view"""
